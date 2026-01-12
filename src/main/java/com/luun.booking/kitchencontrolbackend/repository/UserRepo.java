0756709e36da400ab97d99fb0f9f4edf411fe0f4
luunguyen3112005@gmail.com <luunguyen3112005@gmail.com>
2026-01-12 11:05:55 +0700

    testing web page lastest

---
+++ src/main/java/com/luun.booking/kitchencontrolbackend/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.luun.booking.kitchencontrolbackend.repository;
+
+import com.luun.booking.kitchencontrolbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
